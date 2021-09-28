9830bbeaecd1d6556821f6dd9d7ec92cd16e8971
Ilja Postnovs <Ilja.Postnovs@rimibaltic.com>
2021-09-28 23:42:23 +0300

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,19 @@ import { PositionAdapter } from "./classes/adapters/PositionAdapter";
 import { RangeAdapter } from "./classes/adapters/RangeAdapter";
 import { ILinterConfigHandler } from "./classes/js/parts/config/ILinterConfigHandler";
 import { JSLinterErrorFactory } from "./classes/js/JSLinterErrorFactory";
+import { XMLLinterErrorFactory } from "./classes/xml/XMLLinterErrorFactory";
+import { PropertiesLinterErrorFactory } from "./classes/properties/PropertiesLinterErrorFactory";
+import { PackageConfigHandler } from "./classes/js/parts/config/PackageConfigHandler";
 
 export {
 	Severity,
 	Linter,
 	JSLinter,
+	PackageConfigHandler,
 	ILinterConfigHandler,
 	PositionAdapter,
 	RangeAdapter,
-	JSLinterErrorFactory
+	JSLinterErrorFactory,
+	XMLLinterErrorFactory,
+	PropertiesLinterErrorFactory
 }
